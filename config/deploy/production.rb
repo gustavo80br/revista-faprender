c75382f6df31a8d3e69e53bd932078b0c8d613c5
Gustavo Goncalves <gustavo@faprender.org>
2015-04-15 22:36:01 -0300

    Adjusting deployment scripts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,20 +5,23 @@ set :stage, :production
 #role :app, %w{deploy@example.com}
 #role :web, %w{deploy@example.com}
 #role :db,  %w{deploy@example.com}
+role :app, %w{ubuntu@ec2-52-5-168-163.compute-1.amazonaws.com}
+role :web, %w{ubuntu@ec2-52-5-168-163.compute-1.amazonaws.com}
+role :db, %w{ubuntu@ec2-52-5-168-163.compute-1.amazonaws.com}
 
 # Extended Server Syntax
 # ======================
-server 'example.com', user: 'deploy', roles: %w{web app db}
+server 'ec2-52-5-168-163.compute-1.amazonaws.com', user: 'ubuntu', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-#  set :ssh_options, {
-#    keys: %w(~/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+
+set :ssh_options, {
+    keys: ["#{ENV['USERPROFILE']}/.ssh/faprender-edx-aws.key"],
+    forward_agent: false,
+}
 
 fetch(:default_env).merge!(wp_env: :production)
 
